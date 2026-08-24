a4cee22ffc5ffd3a33346c064ded07e18eb016af
Kauan Lucas Fernandes Damasceno Machado <kauan.11877553@aluno.mg.gov.br>
2026-08-24 07:32:30 -0300

    feat: adicionar arrays de candidatos e matriz de votos

--- SistemaVotacao.java
+++ SistemaVotacao.java
@@ -8,4 +8,14 @@ public class SistemaVotacao {
         System.out.println("Sistema de votação iniciado.");
         scanner.close();
     }
+
+    static final int maxCandidatos = 5;
+    static final int totalTurmas = 3;
+    static final int maxPorTurma = 10;
+    int[] numCandi = new int[maxCandidatos];
+    String[] nomCandi = new String[maxCandidatos];
+    int[] votCandi = new int[maxCandidatos];
+    int[][] votPorTurma = new int[totalTurmas][maxPorTurma];
+    int[] quantiVtTurma = new int[totalTurmas];
+    int quantiCandi = 0;
 }
