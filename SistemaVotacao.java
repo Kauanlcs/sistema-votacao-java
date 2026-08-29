f92f43f157af2abb8a74fa2c5f6070ac133bcbe6
Kauan <kauan.11877553@aluno.mg.gov.br>
2026-08-29 20:20:21 -0300

    feat: adicionar arrays de candidatos e matriz de votos

--- SistemaVotacao.java
+++ SistemaVotacao.java
@@ -7,6 +7,13 @@ public class SistemaVotacao {
     static final int TOTAL_TURMAS = 3;
     static final int MAX_VOTANTES_POR_TURMA = 10;
 
+    int[] numCandi = new int[MAX_CANDIDATOS];
+    String[] nomCandi = new String[MAX_CANDIDATOS];
+    int[] votCandi = new int[MAX_CANDIDATOS];
+    int[][] votPorTurma = new int[TOTAL_TURMAS][MAX_VOTANTES_POR_TURMA];
+    int[] quantiVtTurma = new int[TOTAL_TURMAS];
+    int quantiCandi = 0;
+
     public static void main(String[] args) {
         System.out.println("Sistema de votação iniciado.");
         scanner.close();
